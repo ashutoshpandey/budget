fee48099c2ac9cfc8c1fc2e6053c81989699894f
Ashutosh Pandey <ashutosh.options4u@gmail.com>
2016-01-14 10:21:08 +0530

    async call logic added

--- app/src/main/java/com/budget/buddy/MainActivity.java
+++ app/src/main/java/com/budget/buddy/MainActivity.java
@@ -5,6 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.budget.buddy.data.Utility;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import buddy.budget.com.budgetbuddy.R;
 
@@ -14,6 +23,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadCustomerData();
     }
 
     @Override
@@ -41,6 +52,54 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+/***************************************** custom methods ************************************/
+
+    private void loadCustomerData() {
+
+        RequestParams params = new RequestParams();
+        params.put("id", 1);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(Utility.getData("site_url") + "/useraccount/login/dologin",params ,new AsyncHttpResponseHandler() {
+            // When the response returned by REST has Http response code '200'
+            @Override
+            public void onSuccess(String response) {
+                try {
+                    // JSON Object
+                    JSONObject obj = new JSONObject(response);
+                    // When the JSON response has status boolean value assigned with true
+                    if(obj.getBoolean("status")){
+                        Toast.makeText(getApplicationContext(), "You are successfully logged in!", Toast.LENGTH_LONG).show();
+                    }
+                    // Else display error message
+                    else{
+                        Toast.makeText(getApplicationContext(), obj.getString("error_msg"), Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+            // When the response returned by REST has Http response code other than '200'
+            @Override
+            public void onFailure(int statusCode, Throwable error,
+                                  String content) {
+                // When Http response code is '404'
+                if(statusCode == 404){
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code is '500'
+                else if(statusCode == 500){
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code other than 404, 500
+                else{
+                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     private void createBudget() {
---
+++ app/src/main/java/com/budget/buddy/data/Utility.java
@@ -0,0 +1,35 @@
+package com.budget.buddy.data;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by Ashutosh on 1/14/2016.
+ */
+public class Utility {
+    private static Properties projectProperties;
+
+    static{
+        projectProperties = new Properties();
+
+        try {
+            InputStream in = Utility.class.getResourceAsStream("com.budget.buddy.data.settings");
+            projectProperties.load(in);
+            in.close();
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public static String getData(String key){
+
+        if(projectProperties.isEmpty())
+            return null;
+
+        if(projectProperties.containsKey(key))
+            return projectProperties.getProperty(key);
+        else
+            return null;
+    }
+}
